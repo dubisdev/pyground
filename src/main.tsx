12c40ddc201a44d88f014622ee69386e5a2c4912
dubisdev <dubisdev@gmail.com>
2023-07-09 19:49:04 +0200

    perf: improve load time

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import * as xPython from "@x-python/core";
 
+xPython.init();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-
-await xPython.init();
