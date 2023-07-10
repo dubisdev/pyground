8e6c1dd114e58f94fb369f51cef0d2ba7140776d
dubisdev <dubisdev@gmail.com>
2023-07-10 14:22:38 +0200

    fix: correclty calculate booleans

--- src/services/runpy.ts
+++ src/services/runpy.ts
@@ -6,11 +6,17 @@ export const runpy = async (code: string) => {
     try {
         const executionResult = await exec({ code })
         return {
-            result: executionResult.result ? String(executionResult.result) : "",
+            result: calculateResultOutput(executionResult.result),
             stdout: executionResult.stdout || "",
             stderr: executionResult.stderr
         }
     } catch (e) {
         return { error: String(e) }
     }
+}
+
+const calculateResultOutput = (result: any) => {
+    if (result === null || result === undefined) return ""
+    if (typeof result === "boolean") return result ? "True" : "False"
+    return String(result)
 }
